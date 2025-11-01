38ebd230a91339a864239a2453ff338c76e7ddc1
{sumeyyekocagoz} <sumeyyekocagozz@gmail.com>
2025-11-01 10:32:25 +0000

    Geçici test uygulaması ekle

---
+++ app.py
@@ -0,0 +1,5 @@
+
+import streamlit as st
+st.title("Test Başarılı - Artık Panelinize Girebilirsiniz")
+st.success("Bu uygulamayı birazdan sileceğiz.")
+st.write("Lütfen şimdi Streamlit panelinize gidin ve bozuk olan uygulamayı silin.")
